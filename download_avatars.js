f8c1deab5ae950b000881a548326841e5c2fd18e
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-03-20 01:16:09 +0000

    Complete the function that gets the contributors of a given public GitHub repo

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,33 @@
-var reuqest = require('request');
+var request = require('request');
+var githubToken = require('secrets');
+
 
 console.log("welcome to the GitHub Avatar Downloader!");
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': githubToken
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+
+getRepoContributors('jquery', 'jquery', function(err, result) {
+  console.log("errors:", err);
+  console.log("Result:", result);
+});
+
+  // request.get('https://api.github.com/repos/jquery/jquery/contributors')
+
+// https://api.github.com/users/lighthouse-labs
+// :owner and :repo are jquery
+// "avatar_url
+
+//jquery API to interact with the DOM
