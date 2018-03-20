96cda4699a27891688c1ab195d78be1ec58f41c7
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-03-20 03:11:54 +0000

    Editorial changes

--- download_avatars.js
+++ download_avatars.js
@@ -1,33 +1,41 @@
 var input = process.argv.slice(2);
-
 var request = require('request');
-console.log("welcome to the GitHub Avatar Downloader!");
 
-// Make a request for JSON, getting back a string representation of an array of contributors
+// Make a request for JSON to get back a string representation of an array of GitHub repo contributors.
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  // Make token secret to outsiders.
-  var githubToken = require('./secrets');
+  // Make the function work only if the user inputs the repo owner and the repo name.
+  if (repoOwner && repoName) {
 
-  var options = {
-    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': 'githubToken',
-      'json': 'true'
-    }
-  };
+    console.log("welcome to the GitHub Avatar Downloader!");
 
-  request(options, function(err, res, body) {
-    cb(err, body);
-  });
+    // Make token secret to outsiders.
+    var githubToken = require('./secrets');
+
+    var options = {
+      url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+      headers: {
+        'User-Agent': 'request',
+        'Authorization': 'githubToken',
+        'json': 'true'
+      }
+    };
+
+    request(options, function(err, res, body) {
+      cb(err, body);
+    });
+
+    // If the user does not specify the repo owner and the repo name, print an error message.
+    } else {
+      console.log("Error: you need to specify the repository owner and the repository name, e.g., Lighthouse-labs iOS_Swift_1_Playground");
+  }
 }
 
-// Download an image from a given URL.
+// Download an image from a given URL into the given file path.
 function downloadImageByURL(url, filePath) {
   var fs = require('fs');
 
-  // GET request for url.
+  // Get the url from the server.
   request.get(url)
     .on('error', function(err) {
       throw err;
@@ -39,7 +47,7 @@ function downloadImageByURL(url, filePath) {
     // When the piping is done, print that the download in complete.
     .on('finish', function() {
       console.log('Download complete');
-    });
+  });
 }
 
 // Callback function that saves the avatar image and prints which image of how many is downloading.
@@ -48,10 +56,10 @@ function getAvatar(err, str) {
   // Given that the reponse is a string, we need to parse it to convert it into an array of objects.
   var arr = JSON.parse(str);
 
-  // Looping through each object.
+  // Loop through each object.
   arr.forEach(function(element) {
 
-    // To fetch each object's avatar.
+    // Fetch each object's avatar.
     downloadImageByURL(element.avatar_url, "./avatars/" + element.login + ".jpeg");
 
     // Print which image of how many is downloading.
@@ -59,7 +67,5 @@ function getAvatar(err, str) {
   });
 }
 
+// Invoke the function with the first two arguments provided by the user.
 getRepoContributors(input[0], input[1], getAvatar);
-
-// e.g., 'Lighthouse-labs', 'iOS_Swift_1_Playground'
-
