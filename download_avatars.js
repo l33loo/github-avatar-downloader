1ae56e79c7e790b81d1b421aba4028464ef604bf
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-03-20 02:47:04 +0000

    Add functional version of GitHub Avatar Downloader

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,12 @@
 var request = require('request');
-var githubToken = require('./secrets');
-
-
 console.log("welcome to the GitHub Avatar Downloader!");
 
+// Make a request for JSON, getting back a string representation of an array of contributors
 function getRepoContributors(repoOwner, repoName, cb) {
+
+  // Make token secret to outsiders.
+  var githubToken = require('./secrets');
+
   var options = {
     url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
@@ -19,28 +21,40 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
+// Download an image from a given URL.
+function downloadImageByURL(url, filePath) {
+  var fs = require('fs');
+
+  // GET request for url.
+  request.get(url)
+    .on('error', function(err) {
+      throw err;
+    })
+
+    // Pipe (write) the url into a file.
+    .pipe(fs.createWriteStream(filePath))
+
+    // When the piping is done, print that the download in complete.
+    .on('finish', function() {
+      console.log('Download complete');
+    });
+}
+
+// Callback function that saves the avatar image and prints which image of how many is downloading.
 function getAvatar(err, str) {
+
+  // Given that the reponse is a string, we need to parse it to convert it into an array of objects.
   var arr = JSON.parse(str);
-  // console.log(arr);
+
+  // Looping through each object.
   arr.forEach(function(element) {
-    console.log(element.avatar_url);
-    // console.log(element);
+
+    // To fetch each object's avatar.
+    downloadImageByURL(element.avatar_url, "./avatars/" + element.login + ".jpeg");
+
+    // Print which image of how many is downloading.
+    console.log('Downloading image ' + (arr.indexOf(element) + 1) + ' of ' + arr.length);
   });
 }
 
-
-
 getRepoContributors('Lighthouse-labs', 'iOS_Swift_1_Playground', getAvatar);
-
-// getRepoContributors('jquery', 'jquery', function(err, result) {
-//   console.log("errors:", err);
-//   console.log("Result:", result);
-// });
-
-  // request.get('https://api.github.com/repos/jquery/jquery/contributors')
-
-// https://api.github.com/users/lighthouse-labs
-// :owner and :repo are jquery
-// "avatar_url
-
-//jquery API to interact with the DOM
